28da06be6e0ba38fd06b1501f90cd550483992c0
jihye <zehye.01@gmail.com>
2019-12-17 17:19:22 +0900

    detail, results, vote view 작성

--- polls/urls.py
+++ polls/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
-from .views import index
+from .views import index, detail, results, vote
 
 
 urlpatterns = [
     path('', index, name='index'),
+    path('<int:question_id>/', detail, name='detail'),
+    path('<int:question_id>/results/', results, name='results'),
+    path('<int:question_id>/vote/', vote, name='vote'),
 ]
--- polls/views.py
+++ polls/views.py
@@ -4,3 +4,16 @@ from django.shortcuts import render
 
 def index(request):
     return HttpResponse('Hello, world.')
+
+
+def detail(request, question_id):
+    return HttpResponse(f"You're looking at question {question_id}")
+
+
+def results(request, question_id):
+    response = "you're looking at the results of question{}"
+    return HttpResponse(response.format(question_id))
+
+
+def vote(request, question_id):
+    return HttpResponse(f"You're voting on question {question_id}")
