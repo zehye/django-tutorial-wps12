fdaf740f316609466d5f46f1c97cb83475f97b52
jihye <zehye.01@gmail.com>
2019-12-17 16:22:57 +0900

    Question, Choice 모델 및 마이그레이션 실행

--- polls/models.py
+++ polls/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+
+class Question(models.Model):
+    question_text = models.CharField(max_length=200)
+    pub_date = models.DateTimeField('data published')
+
+
+class Choice(models.Model):
+    # choice 여러개에 question 하나를 연결 -> foreignkey
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    choice_text = models.CharField(max_length=200)
+    votes = models.IntegerField(default=0)
