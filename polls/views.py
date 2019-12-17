ac67f06f86434a22bcbe3471707c4d1c4dade303
jihye <zehye.01@gmail.com>
2019-12-17 17:35:02 +0900

    polls/index view에 맞는 templates/index.html 작성

--- polls/views.py
+++ polls/views.py
@@ -1,9 +1,12 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from .models import Question, Choice
 
 
 def index(request):
-    return HttpResponse('Hello, world.')
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    output = ' '.join([q.question_text for q in latest_question_list])
+    return HttpResponse(output)
 
 
 def detail(request, question_id):
