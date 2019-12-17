70111c06f56c5fc7ada07fb4a0340ae942be46d8
jihye <zehye.01@gmail.com>
2019-12-17 17:47:50 +0900

    detail구현, DoesNotExist 발생시 404페이지 출력

--- polls/views.py
+++ polls/views.py
@@ -1,4 +1,4 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, Http404
 from django.shortcuts import render
 from django.template import loader
 
@@ -7,15 +7,25 @@ from .models import Question, Choice
 
 def index(request):
     latest_question_list = Question.objects.order_by('-pub_date')[:5]
-    template = loader.get_template('polls/index.html')
+    # template = loader.get_template('polls/index.html')
     context = {
         'latest_question_list': latest_question_list,
     }
-    return HttpResponse(template.render(context, request))
+    return render(request, 'polls/index.html', context)
+    # return HttpResponse(template.render(context, request))
 
 
 def detail(request, question_id):
-    return HttpResponse(f"You're looking at question {question_id}")
+    try:
+        question = Question.objects.get(pk=question_id)
+    except Question.DoesNotExist:
+        raise Http404('Question does not exist!!!')
+
+    context = {
+        'question': question,
+    }
+    # return HttpResponse(f"You're looking at question {question_id}")
+    return render(request, 'polls/detail.html', context)
 
 
 def results(request, question_id):
