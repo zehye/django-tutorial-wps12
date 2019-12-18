e57b7f890402528fc5b961e0fe523a4e4981076e
jihye <zehye.01@gmail.com>
2019-12-18 13:21:56 +0900

    detail view작성과 이에 맞는 templates작성.

--- polls/views.py
+++ polls/views.py
@@ -1,5 +1,5 @@
 from django.http import HttpResponse, Http404
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.template import loader
 
 from .models import Question, Choice
@@ -16,11 +16,12 @@ def index(request):
 
 
 def detail(request, question_id):
-    try:
-        question = Question.objects.get(pk=question_id)
-    except Question.DoesNotExist:
-        raise Http404('Question does not exist!!!')
-
+    # try:
+    #     question = Question.objects.get(pk=question_id)
+    # except Question.DoesNotExist:
+    #     raise Http404('Question does not exist!!!')
+    #
+    question = get_object_or_404(Question, pk=question_id)
     context = {
         'question': question,
     }
