ae8e6e39b06df5874e9bc821c267ebe33a16e81f
jihye <zehye.01@gmail.com>
2019-12-18 13:27:39 +0900

    url namespace 사용

--- polls/urls.py
+++ polls/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 from .views import index, detail, results, vote
 
-
+app_name = 'polls'
 urlpatterns = [
     path('', index, name='index'),
     path('<int:question_id>/', detail, name='detail'),
